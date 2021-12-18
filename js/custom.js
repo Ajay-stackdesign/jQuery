de5cfc8be0de93f971ad4bf6c327f10fc2c40bee
Ajay-stackdesign <ajaygsahani968@gmail.com>
2021-12-18 22:59:14 +0530

    changes

--- js/custom.js
+++ js/custom.js
@@ -51,57 +51,57 @@ jQuery(document).ready(($) => {
     })
 })
 
-jQuery.noConflict();
-jQuery(document).ready(($) => {
-    $("#username").focus(() => {
-        console.log("blur")
-    })
-    $("#form-submit").submit((e) => {
-        console.log("form subitted")
-        e.preventDefault()
-    })
-    $(window).resize(function(){
-        console.log("wondow resize")
-    })
-})
+// jQuery.noConflict();
+// jQuery(document).ready(($) => {
+//     $("#username").focus(() => {
+//         console.log("blur")
+//     })
+//     $("#form-submit").submit((e) => {
+//         console.log("form subitted")
+//         e.preventDefault()
+//     })
+//     $(window).resize(function(){
+//         console.log("wondow resize")
+//     })
+// })
 
-jQuery.noConflict();
-jQuery(document).ready(($) => {
-    $("#btn-hide").click(() => {
-        $("#image-id").hide(5000,() => {
-            console.log("imahe is")
-        });
-    })   
-    $("#btn-show").click(() => {
-        $("#image-id").show(5000,() => {
-            console.log("hi show ")
-        })
-    }) 
-    $("#btn-toggle").click(() => {
-        $("#image-id").toggle(() => {
-            console.log("hide/show")
-        })
-    })
-})
+// jQuery.noConflict();
+// jQuery(document).ready(($) => {
+//     $("#btn-hide").click(() => {
+//         $("#image-id").hide(5000,() => {
+//             console.log("imahe is")
+//         });
+//     })   
+//     $("#btn-show").click(() => {
+//         $("#image-id").show(5000,() => {
+//             console.log("hi show ")
+//         })
+//     }) 
+//     $("#btn-toggle").click(() => {
+//         $("#image-id").toggle(() => {
+//             console.log("hide/show")
+//         })
+//     })
+// })
 
-jQuery.noConflict();
-jQuery(document).ready(($) =>{
-    $("#btn-fadeIn").click(() => {
-        $("#image-id").fadeIn(() => {
-            console.log("fade in")
-        })
-    })
-    $("#btn-fadeTo").click(() => {
-        $("#image-id").fadeTo(1000,0.5,() => {
-            console.log("fade To")
-        })
-    })
-    // $("#btn-toggle").click(() => {
-    //     $("#image-id").fadeToggle(() => {
-    //         console.log("fade in")
-    //     })
-    // })
-})
+// jQuery.noConflict();
+// jQuery(document).ready(($) =>{
+//     $("#btn-fadeIn").click(() => {
+//         $("#image-id").fadeIn(() => {
+//             console.log("fade in")
+//         })
+//     })
+//     $("#btn-fadeTo").click(() => {
+//         $("#image-id").fadeTo(1000,0.5,() => {
+//             console.log("fade To")
+//         })
+//     })
+//     // $("#btn-toggle").click(() => {
+//     //     $("#image-id").fadeToggle(() => {
+//     //         console.log("fade in")
+//     //     })
+//     // })
+// })
 
 // jQuery.noConflict();
 // jQuery(document).ready(($) => {
@@ -112,46 +112,46 @@ jQuery(document).ready(($) =>{
 //     })
 // })
 
-jQuery.noConflict();
-jQuery(document).ready(($) => {
-    $("#slideUp").click(() => {
-        $("#image-id").slideUp(() => {
-            console.log("sldie up")
-        })
-    })
-    $("#slideDown").click(() => {
-        $("#image-id").slideDown(() => {
-            console.log("slide Down")
-        })
-    })
-    $("#slideToggle").click(() => {
-        $("#image-id").slideToggle(() => {
-            console.log("slide toggle")
-        })
-    })
-})
-jQuery.noConflict();
-jQuery(document).ready(($) => {
-    $("#zom-id").click(() => {
-        $("#zombie").animate({left: "+=80"},() => {
-            console.log("zombie animation")
-        })
-    })
-    //get text area
-    let text = $("p").text();
-    console.log(text);
+// jQuery.noConflict();
+// jQuery(document).ready(($) => {
+//     $("#slideUp").click(() => {
+//         $("#image-id").slideUp(() => {
+//             console.log("sldie up")
+//         })
+//     })
+//     $("#slideDown").click(() => {
+//         $("#image-id").slideDown(() => {
+//             console.log("slide Down")
+//         })
+// //     })
+//     $("#slideToggle").click(() => {
+//         $("#image-id").slideToggle(() => {
+//             console.log("slide toggle")
+//         })
+//     })
+// })
+// jQuery.noConflict();
+// jQuery(document).ready(($) => {
+//     $("#zom-id").click(() => {
+//         $("#zombie").animate({left: "+=80"},() => {
+//             console.log("zombie animation")
+//         })
+//     })
+//     //get text area
+//     let text = $("p").text();
+//     console.log(text);
 
 
-    //get text
-   // let textArea = $("p").text("hello world")
-    //console.log(textArea);
+//     //get text
+//    // let textArea = $("p").text("hello world")
+//     //console.log(textArea);
 
-    //get and set value sing val();
+//     //get and set value sing val();
 
-    let value = $("#name").val()
-    console.log(value)
+//     let value = $("#name").val()
+//     console.log(value)
 
-    $("#btn1").click(() => {
-        $("#name").val("ajay sahani")
-    })
-})
+//     $("#btn1").click(() => {
+//         $("#name").val("ajay sahani")
+//     })
+// })
