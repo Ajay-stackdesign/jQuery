1df0d3aa4a567f025eea3bcf46f4f604b33b6704
Ajay-stackdesign <ajaygsahani968@gmail.com>
2022-03-16 23:31:10 +0530

    changes

---
+++ js/js/index.js
@@ -0,0 +1,16 @@
+console.log(x)
+var x = 10
+
+getName()
+console.log(x)
+console.log("arrow")
+const getName = () => {
+    console.log(10)
+}
+
+console.log("simple function")
+// function getName() {
+//     console.log(10)
+// }
+
+var x = 10
